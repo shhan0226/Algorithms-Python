be426a403bd97787d19c3f1f451e9db1dcfd2941
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2020-06-06 22:07:27 +0900

    Add files via upload

---
+++ Pytube/downpy.py
@@ -0,0 +1,34 @@
+from pytube import YouTube
+
+def download_(urllsit):
+    urltag = "https://www.youtube.com/watch?v="
+    for url_ in urllsit :
+        print(urltag + url_)
+        urlsum = YouTube(urltag + url_)
+        urlstream = urlsum.streams.filter(progressive=True)
+        print("Title : " + urlsum.title)
+
+        urlcount = -1
+        for i in range(len(urlstream)):
+            urlcount += 1
+            print(i, '.', urlstream[i])
+
+        dir_ = "D:/VShare"
+        urlstream[urlcount].download(dir_)
+
+def file_():
+    urls = []
+    f = open("input", 'r')
+    lines = f.readlines()
+    for line in lines:
+        strs = line.strip("\n").split('?v=')
+        url_ = strs[1]
+        urls.append(url_)
+    f.close()
+    return urls
+
+if __name__ == '__main__':
+    str_ = file_()
+    download_(str_)
+    print("end")
+
